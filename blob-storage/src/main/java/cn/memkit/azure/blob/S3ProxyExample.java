90434f00312058052b52dc132981587b4d593dda
MemKit <meadking@qq.com>
2024-10-08 17:51:55 +0800

    Update S3ProxyExample.java

--- blob-storage/src/main/java/cn/memkit/azure/blob/S3ProxyExample.java
+++ blob-storage/src/main/java/cn/memkit/azure/blob/S3ProxyExample.java
@@ -7,7 +7,12 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-
+/**
+* curl --request PUT http://127.0.0.1:80/testbucket
+* curl http://127.0.0.1:80/
+* https://github.com/gaul/s3proxy/blob/master/Dockerfile
+* https://hub.docker.com/r/andrewgaul/s3proxy?uuid=D66BBA35-9F7F-4A6B-8D17-AC9BEDC195C1
+**
 public class S3ProxyExample {
     public static void main(String[] args) {
         String s3ProxyEndpoint = "http://127.0.0.1:80";
